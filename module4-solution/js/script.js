350f8ec03ef44ced95ef3ba777bf77a021974f56
kr.viku <er.vikashs4@gmail.com>
2020-06-06 23:41:05 +0530

    Create Script.js

---
+++ module4-solution/js/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
