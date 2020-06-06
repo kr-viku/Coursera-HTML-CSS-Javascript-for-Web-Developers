b29e38aa5ac74623ccea8abd1bc952939595daac
kr.viku <er.vikashs4@gmail.com>
2020-06-06 23:41:35 +0530

    create hello.js

---
+++ module4-solution/js/SpeakHello.js
@@ -0,0 +1,9 @@
+(function (window) {  
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+    
+    helloSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+        window.helloSpeaker = helloSpeaker;
+    })(window);
